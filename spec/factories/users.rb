fd332b8a63478d05645bf45bb648fde8e9e15eff
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-02-04 13:53:06 +0000

    Added FactoryGirl fb login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
+    email 'alex@alex.com'
+    password 'password'
+    password_confirmation { password }
+    provider 'facebook'
+    uid '1234567890'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,12 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rails'
+require 'factory_girl'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -15,9 +17,13 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
 
+  config.include FactoryGirl::Syntax::Methods
+  config.include Warden::Test::Helpers
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    Warden.test_mode!
   end
 
   config.before(:each) do
