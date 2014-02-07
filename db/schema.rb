b90b4d83e73f67c1019132c013d2bf325520ba87
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-02-07 17:29:47 +0000

    Removed unnecessary photo spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140207105709) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +25,11 @@ ActiveRecord::Schema.define(version: 20140207105709) do
 
   add_index "mixtapes", ["user_id"], name: "index_mixtapes_on_user_id", using: :btree
 
+  create_table "mixtapes_tracks", id: false, force: true do |t|
+    t.integer "mixtape_id", null: false
+    t.integer "track_id",   null: false
+  end
+
   create_table "photos", force: true do |t|
     t.integer  "user_id"
     t.string   "image_url"
@@ -36,11 +39,6 @@ ActiveRecord::Schema.define(version: 20140207105709) do
   end
 
   add_index "photos", ["user_id"], name: "index_photos_on_user_id", using: :btree
-
-  create_table "mixtapes_tracks", id: false, force: true do |t|
-    t.integer "mixtape_id", null: false
-    t.integer "track_id",   null: false
-  end
 
   create_table "tracks", force: true do |t|
     t.datetime "created_at"
@@ -69,7 +67,6 @@ ActiveRecord::Schema.define(version: 20140207105709) do
     t.string   "location"
     t.string   "birthday"
     t.string   "auth_token"
-    t.boolean  "gender"
     t.boolean  "male"
   end
 
