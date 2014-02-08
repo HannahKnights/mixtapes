0c8e23b6792c2baad88673a284ddb7e90e4e07b3
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-02-08 21:47:49 +0000

    Fixed redirect omniauth authentication to close pop-up

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,6 +11,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+  
   end
 
 
