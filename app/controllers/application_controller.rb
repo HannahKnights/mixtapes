a225fa109086e5607a2c25b0476663d928347aae
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-02-08 15:08:25 +0000

    Adding 'sign in with fb' to new mixtape view. Rendered in pop-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,17 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:account_update) << parameter
     end
   end
+
+  def devise_mapping
+    @devise_mapping ||= request.env["devise.mapping"]
+  end
+
+  def resource_name
+    @devise_mapping.name
+  end
+
+  def resource_class
+    @devise_mapping.to
+  end
   
 end
