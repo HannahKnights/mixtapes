b50bc1c40498b1d3bb723a4fcc40bef74efab9db
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-02-12 12:33:40 +0000

    Bug fixing adding class and data-attribute on update, raising flash fail

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,11 +5,12 @@ class PhotosController < ApplicationController
     user_photos = Photo.where(user_id: current_user.id)
     photo = current_user.photos.find_by image_url: params[:image_url]
     if user_photos.where(profile_picture: true).count < 5 && photo.profile_picture != true 
-      photo.update_attributes( profile_picture: true )
+      photo.update_attributes( profile_picture: true ) 
     else
       flash[:error] = "Sorry we cannot save that picture!"
+      raise[:error]
     end
-    render json: { status: 'success' }
+    render json: { status: 'success', id: photo.id }
   end
 
   def destroy
@@ -17,10 +18,12 @@ class PhotosController < ApplicationController
     photo = Photo.find(params[:id])
     if user_photos.where(profile_picture: true).count > 1
       photo.update_attributes( profile_picture: false )
+      render json: { status: 'success' }
     else
-      flash[:error] = "Sorry you must have more than one picture!"
+      render json: { status: 'Error', message: "Sorry you must have more than one picture!" }, status: 401
+      # flash[:error] = "Sorry you must have more than one picture!"
+      # raise[:error]
     end
-    render json: { status: 'success' }
   end
 
 end
