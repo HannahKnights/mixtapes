7d7b6080245befc611c49307d1f0c3fedb3712ee
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-02-12 14:18:30 +0000

    Adding echonest_song_id to matching method

--- app/models/mixtape.rb
+++ app/models/mixtape.rb
@@ -9,8 +9,8 @@ class Mixtape < ActiveRecord::Base
   def match_rate(match_mixtape)
     me = self.tracks
     you = match_mixtape
-    me_song, me_artist, me_genre = mix_values(me, 'id') , mix_values(me, 'artist'), mix_values(me, 'genre')
-    you_song, you_artist, you_genre = mix_values(you, 'id') , mix_values(you, 'artist'), mix_values(you, 'genre')
+    me_song, me_artist, me_genre = mix_values(me, 'echonest_song_id') , mix_values(me, 'artist'), mix_values(me, 'genre')
+    you_song, you_artist, you_genre = mix_values(you, 'echonest_song_id') , mix_values(you, 'artist'), mix_values(you, 'genre')
     result = (((me_genre & you_genre).count * 2) + 
               ((me_song & you_song).count * 3) + 
               ((me_artist & you_artist).count * 1))
